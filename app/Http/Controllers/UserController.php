6b39bb42bb8c51c9e99e0b30ead63ed3edbd45d3
Hideki Kamakura <kakihk695@gmail.com>
2020-12-25 17:01:19 +0900

    fix(UserController):不要関数削除

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -4,16 +4,13 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\User;
-use App\Hero;
 
 class UserController extends Controller
 {
  public   function index(){
     return User::all();
    }
- public   function getHeroes(){
-    return Hero::all();
-   }
+
 
    public function add(Request $request) {
 
@@ -25,31 +22,5 @@ class UserController extends Controller
     $user->save();
 
     return true;
-}
-   public function hero(Request $request) {
-
-    $hero = new Hero();
-    $hero->id = $request->id;
-    $hero->name = $request->name;
-    $hero->save();
-
-    return redirect('/getHero');
-}
-
-public function update(Request $request, $id)
-{
-    $hero = Hero::findOrFail($id);
-    $hero->name = $request->name;
-    $hero->save();
-
-    return 'updated';
-}
-
-public function delete($id)
-{
-    $hero = Hero::findOrFail($id);
-    $hero->delete();
-
-    return 'deleted!';
 }
 }
